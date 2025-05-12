8dcd877cbb48afa013fec3f64824f2c116165dc0
Adam Beguelin <adam@beguelin.com>
2025-05-12 16:10:36 +0700

    make sender code.pr

--- functions/email.js
+++ functions/email.js
@@ -16,7 +16,7 @@ export async function sendEmail({ to, subject, text, html, redirectUrl, MAILHOP_
     messages: [{
       from: { 
         name: "code.pr",
-        email: 'noreply@mailhop.org' 
+        email: 'noreply@code.pr' 
       },
       to: [{ 
         email: to 
@@ -51,7 +51,7 @@ export async function sendEmail({ to, subject, text, html, redirectUrl, MAILHOP_
         messages: [{
           from: { 
             name: "code.pr",
-            email: 'noreply@mailhop.org' 
+            email: 'noreply@code.pr' 
           },
           to: [{ 
             email: to 
